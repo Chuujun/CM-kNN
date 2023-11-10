43c293fc58ed6ba798a60a7a8f16c6d1243840a9
Chujun <butterccj@outlook.com>
2023-11-10 12:22:48 +0800

    add l_knn, ll_knn into test

--- kfold_cross_validation.py
+++ kfold_cross_validation.py
@@ -6,7 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier as KNN
 
 from cm_knn import cm_knn
 from data_preprocess import normalize_data_minmax
-from baseline import cv_knn, ad_knn, knn, lmnn
+from baseline import cv_knn, l_knn, ll_knn, ad_knn, knn, lmnn
 
 def run_KFold(k, random, features, labels):
     # Setting up K-Fold Cross-Validation
@@ -15,9 +15,12 @@ def run_KFold(k, random, features, labels):
     # KFold
     CM_KNN_accuracy_list = []
     KNN_accuracy_list = []
-    AD_KNN_accuracy_list = []
+    L_KNN_accuracy_list = []
+    LL_KNN_accuracy_list = []
     CV_KNN_accuracy_list = []
+    AD_KNN_accuracy_list = []
     LMNN_accuracy_list = []
+
     for i, (train_index, test_index) in enumerate(kf.split(features)):
         # Splitting the data into training and testing sets
         X, Y = features[train_index], features[test_index]
@@ -38,10 +41,20 @@ def run_KFold(k, random, features, labels):
         print("KNN_accuracy: ", KNN_accuracy)
         KNN_accuracy_list.append(KNN_accuracy)
 
-        # CM_KNN test
+        # CV_KNN test
         CV_KNN_accuracy = cv_knn(X_normalized, Y_normalized, X_label, Y_label)
         print("CV_KNN_accuracy: ", CV_KNN_accuracy)
         CV_KNN_accuracy_list.append(CV_KNN_accuracy)
+
+        # L_KNN test
+        L_KNN_accuracy = l_knn(X_normalized, Y_normalized, X_label, Y_label)
+        print("L_KNN_accuracy: ", L_KNN_accuracy)
+        L_KNN_accuracy_list.append(L_KNN_accuracy)
+
+        # LL_KNN test
+        LL_KNN_accuracy = ll_knn(X_normalized, Y_normalized, X_label, Y_label)
+        print("LL_KNN_accuracy: ", LL_KNN_accuracy)
+        LL_KNN_accuracy_list.append(LL_KNN_accuracy)
 
         # AD_KNN test
         AD_KNN_accuracy = ad_knn(X_normalized, Y_normalized, X_label, Y_label)
